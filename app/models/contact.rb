71123d60e2465eaf0f50a2cb817b8f190513ea86
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-10-26 20:13:07 -0300

    chore: include relationship

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 class Contact < ApplicationRecord
-    belongs_to :kind
+  belongs_to :kind
+
+  def author
+    'Felipe Koetz'
+  end
+
+  def kind_description
+    kind.description
+  end
+
+  def as_json(_options = {})
+    super(methods: %i[kind_description author], root: true, include: :kind)
+  end
 end
