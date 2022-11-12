4a842c598a1ba08539de7cee8cfff0e190cb25e7
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-11-12 19:34:59 -0300

    chore: belongs_to relation

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do 
+    link(:related) { kind_url(object.kind.id) }
+  end
+  
   has_many :phones
   has_one :address
+  
+  # link(:self) { contact_url(object.id) }
 
   meta do 
     { author: 'Felipe Henssler Koetz' }
@@ -14,5 +19,4 @@ class ContactSerializer < ActiveModel::Serializer
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
-
 end
