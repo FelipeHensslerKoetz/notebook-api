aa48024faa59df902bffb8e5b8715d0e985564e1
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-11-14 11:16:01 -0300

    chore: ams belongs to pattern

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :kinds
-  resources :contacts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :contacts do 
+    resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
+  end
 end
